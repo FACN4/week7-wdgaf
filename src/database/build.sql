32f654e5713e6eae94b6a3ad9ff1dae7c00dccaa
solk10 <sol.1919@yahoo.com>
2018-08-07 16:21:30 +0300

    database justified > #4

--- src/database/build.sql
+++ src/database/build.sql
@@ -3,28 +3,28 @@ BEGIN;
 DROP TABLE IF EXISTS USERS CASCADE;
 
 CREATE TABLE USERS (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  power TEXT NOT NULL,
-  weight INTEGER DEFAULT 100
+  user_id SERIAL PRIMARY KEY,
+  git_username VARCHAR (255) NOT NULL,
+  git_profile_url VARCHAR (2083) NOT NULL,
+  git_photo_url VARCHAR (2083) NOT NULL,
+  elo_ranking INTEGER DEFAULT 1500
 );
-INSERT INTO superheroes (name,power,weight)
- -- VALUES('spiderMAN','WWW',80),
- -- ('WAKWOK','booktook',70);
-
-COMMIT;
-BEGIN;
+-- INSERT INTO USERS (git_username,git_profile_url,git_photo_url,elo_ranking)
+--  VALUES('spiderMAN','WWW',80),
+--  ('WAKWOK','booktook',70);
 
 DROP TABLE IF EXISTS RATINGS CASCADE;
 
 CREATE TABLE RATINGS (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  power TEXT NOT NULL,
-  weight INTEGER DEFAULT 100
+  rating_id SERIAL PRIMARY KEY,
+  user_id VARCHAR(100) NOT NULL REFERENCES USERS(user_id),
+  winner VARCHAR(255) NOT NULL,
+  opponent_id TEXT NOT NULL,
+  opponent_elo INTEGER DEFAULT 1500,
+
 );
-INSERT INTO superheroes (name,power,weight)
- -- VALUES('spiderMAN','WWW',80),
- -- ('WAKWOK','booktook',70);
+-- INSERT INTO RATINGS (user_id,winner,opponent_id,opponent_elo)
+--  -- VALUES('spiderMAN','WWW',80),
+--  -- ('WAKWOK','booktook',70);
 
 COMMIT;
