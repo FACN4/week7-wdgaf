03d6c2b94e1d928cba1d06d889f50d3da7a96c0f
Matt Hall <matthall8@gmail.com>
2018-08-08 12:27:49 +0300

    Added Walls Handler

--- src/handlers/getWallsHandler.js
+++ src/handlers/getWallsHandler.js
@@ -1,5 +1,29 @@
+const { getUserData } = require('../query/getData');
+
 /* Function to return data for the wall of fame and wall of shame */
 
-const getWallsHandler = (request, response) => {};
+const getWallsHandler = (request, response) => {
+  getUserData((error, result) => {
+    // Error message to send if the data doesn't send correctly
+    if (error) {
+      response.writeHead(500, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ error: 'Sorry, unable to fulfil request' }));
+    } else {
+      // sort the users by their elo score
+      const sortedArray = parseInt(result.row.length, 10);
+      // slice the top 5 and lowest 5
+      const wallOfFame = sortedArray.slice(0, 5);
+      const wallOfShame = sortedArray.slice(-5);
+      // Build object with the 2 users to respond to XHR request
+      const res = {
+        wallOfFame,
+        wallOfShame,
+      };
+      // Send result to frontend
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(res));
+    }
+  });
+};
 
 module.exports = getWallsHandler;
