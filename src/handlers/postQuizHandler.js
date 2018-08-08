aad85c17c047dc2c3194625549b6cc637ed0466c
Matt Hall <matthall8@gmail.com>
2018-08-08 12:53:28 +0300

    Post Quiz Handler

--- src/handlers/postQuizHandler.js
+++ src/handlers/postQuizHandler.js
@@ -2,7 +2,7 @@ const { calcNewElo } = require('../logic.js');
 const { updateElo, logRating } = require('../query/postData');
 
 /* Function to update the database with the results of the quiz */
-const pushQuizHandler = (request, response) => {
+const postQuizHandler = (request, response) => {
   /* Using post method so data is coming in streams */
   let allData = '';
   request.on('data', (data) => {
