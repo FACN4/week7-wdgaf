243e2204866f0768fc3a3d26722f409ffc45ae5d
solk10 <sol.1919@yahoo.com>
2018-08-08 15:22:50 +0300

    url avatar is set

--- src/database/build.js
+++ src/database/build.js
@@ -10,14 +10,16 @@ dbConnection.query(makeEmptyTables, (error) => {
   } else {
     console.log('Buiding DB success');
     userData.forEach((person) => {
-      const SQLquery = 'INSERT INTO users (git_username,git_profile_url,git_photo_url) VALUES ($1,$2,$3)';
-      dbConnection.query(SQLquery, [person.login, person.html_url, person.avatar_url], (err) => {
-        if (err) {
-          console.log('Filling DB error', err);
-        } else {
-          console.log('Filling DB success');
-        }
-      });
+      if (person.avatar_url) {
+        const SQLquery = 'INSERT INTO users (git_username,git_profile_url,git_photo_url) VALUES ($1,$2,$3)';
+        dbConnection.query(SQLquery, [person.login, person.html_url, person.avatar_url], (err) => {
+          if (err) {
+            console.log('Filling DB error', err);
+          } else {
+            console.log('Filling DB success');
+          }
+        });
+      }
     });
   }
 });
