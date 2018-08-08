98164fc4f441310b3fcde4ad32d0cfe8228933bf
solk10 <sol.1919@yahoo.com>
2018-08-08 12:18:49 +0300

    data set > #11

--- src/database/build.js
+++ src/database/build.js
@@ -1,12 +1,23 @@
 const fs = require('fs');
-const dbConnection = require('src/database/dbconnection');
+const dbConnection = require('./dbconnection');
+const userData = require('./git_member_data.json');
 
-const file = fs.readFileSync(__dirname+'src/database/build.sql','utf-8');
+const makeEmptyTables = fs.readFileSync(`${__dirname}/build.sql`, 'utf-8');
 
-dbConnection.query(file,(error,result)=>{
-  if(error){
-    console.log('Error',error);
-  }else{
-    console.log('success');
+dbConnection.query(makeEmptyTables, (error) => {
+  if (error) {
+    console.log('Building DB error', error);
+  } else {
+    console.log('Buiding DB success');
+    userData.forEach((person) => {
+      const SQLquery = 'INSERT INTO users (git_username,git_profile_url,git_photo_url) VALUES ($1,$2,$3)';
+      dbConnection.query(SQLquery, [person.login, person.html_url, person.avatar_url], (err) => {
+        if (err) {
+          console.log('Filling DB error', err);
+        } else {
+          console.log('Filling DB success');
+        }
+      });
+    });
   }
 });
--- src/query/postData.js
+++ src/query/postData.js
@@ -4,7 +4,8 @@ const dbConnection = require('../database/dbconnection.js');
 const updateElo = (userId, eloNew, cb) => {
   // The maximum change in ELO ranking is 200. Additional conditions are put
   // in place to prevent malicous users from giving themselves higher ranking
-  dbConnection.query('UPDATE users SET elo_ranking = $1 WHERE user_id = $2 AND elo_ranking + 200 > $1 AND elo_ranking - 200 < $1', [eloNew, userId], (err) => {
+  const queryString = 'UPDATE users SET elo_ranking = $1 WHERE user_id = $2 AND elo_ranking + 200 > $1 AND elo_ranking - 200 < $1';
+  dbConnection.query(queryString, [eloNew, userId], (err) => {
     if (err) {
       cb(err);
     } else {
@@ -15,7 +16,8 @@ const updateElo = (userId, eloNew, cb) => {
 
 // Logs the event of a rating, called in pushQuizHandler
 const logRating = (user1Id, user1Elo, user2Id, user2Elo, user1Wins, cb) => {
-  dbConnection.query('INSERT INTO ratings (user1_id, user1_elo, user2_id, user2_elo,user1_wins) VALUES ($1, $2, $3, $4, $5)', [user1Id, user1Elo, user2Id, user2Elo, user1Wins], (err) => {
+  const queryString = 'INSERT INTO ratings (user1_id, user1_elo, user2_id, user2_elo,user1_wins) VALUES ($1, $2, $3, $4, $5)';
+  dbConnection.query(queryString, [user1Id, user1Elo, user2Id, user2Elo, user1Wins], (err) => {
     if (err) {
       cb(err);
     } else {
