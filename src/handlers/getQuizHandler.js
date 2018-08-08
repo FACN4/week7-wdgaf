421a283c7b63f469a88e49ed65e80ca8273d33d3
Matt Hall <matthall8@gmail.com>
2018-08-08 11:31:09 +0300

    Updating Quiz Handler

--- src/handlers/getQuizHandler.js
+++ src/handlers/getQuizHandler.js
@@ -1,3 +1,5 @@
+const { getUserData } = require('../query/getData');
+
 /* Function to produce a resposne for 2 random profiles for the quiz */
 const getQuizHandler = (request, response) => {
   getUserData();
