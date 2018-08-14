734671dd661831704479447375c680799f69ad3f
afifisuha <afifisuha@campus.technion.ac.il>
2018-08-14 16:46:43 +0300

    #1 added login and signup pages

--- output/dom.js
+++ output/dom.js
@@ -1,25 +1,33 @@
-'use strict';
+"use strict";
 
 /* global getQuiz postQuizRes getWalls */
 
 // Define a global variable of the users currently on screen
 var globalUsers = void 0;
 
-var user1 = document.getElementById('user1');
-var user1Img = document.getElementById('user1Img');
-var user1Name = document.getElementById('user1').getElementsByTagName('h2')[0];
-var user2 = document.getElementById('user2');
-var user2Img = document.getElementById('user2Img');
-var user2Name = document.getElementById('user2').getElementsByTagName('h2')[0];
+var user1 = document.getElementById("user1");
+var user1Img = document.getElementById("user1Img");
+var user1Name = document.getElementById("user1").getElementsByTagName("h2")[0];
+var user2 = document.getElementById("user2");
+var user2Img = document.getElementById("user2Img");
+var user2Name = document.getElementById("user2").getElementsByTagName("h2")[0];
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var confirmPassword = document.getElementById("confirmPassword");
+var form = document.getElementsByTagName("form")[0];
+
+var emailErr = document.getElementById("emailErr");
+var passwordErr = document.getElementById("passwordErr");
+var confirmErr = document.getElementById("confirmErr");
 
 var warningMessage = function warningMessage(text, delay) {
-  var warningBar = document.getElementById('alert');
+  var warningBar = document.getElementById("alert");
   var warningText = warningBar.childNodes[1];
   warningText.textContent = text;
-  warningBar.style.visibility = 'visible';
+  warningBar.style.visibility = "visible";
   if (delay) {
     setTimeout(function () {
-      warningBar.style.visibility = 'hidden';
+      warningBar.style.visibility = "hidden";
     }, delay);
   }
 };
@@ -34,17 +42,17 @@ var removeChildren = function removeChildren(obj) {
 var newQuiz = function newQuiz() {
   getQuiz(function (err, userObjects) {
     if (err) {
-      warningMessage('Error while getting quiz', 2000);
+      warningMessage("Error while getting quiz", 2000);
     } else {
       globalUsers = userObjects;
       // globaluserindex holds the user's index within the globalUsers variable
-      user1.setAttribute('globaluserindex', '0');
+      user1.setAttribute("globaluserindex", "0");
       user1Name.textContent = userObjects.user1.git_username;
       user1Img.src = userObjects.user1.git_photo_url;
-      user1Img.alt = String(userObjects.user1.git_username) + ' GIT Profile Photo';
-      user2.setAttribute('globaluserindex', '1');
+      user1Img.alt = String(userObjects.user1.git_username) + " GIT Profile Photo";
+      user2.setAttribute("globaluserindex", "1");
       user2Name.textContent = userObjects.user2.git_username;
-      user2Img.alt = String(userObjects.user2.git_username) + ' GIT Profile Photo';
+      user2Img.alt = String(userObjects.user2.git_username) + " GIT Profile Photo";
       user2Img.src = userObjects.user2.git_photo_url;
     }
   });
@@ -53,18 +61,18 @@ var newQuiz = function newQuiz() {
 // Fn to set up the wall of fame/shame
 var generateWallHTML = function generateWallHTML(wallObj, ulId, heading) {
   removeChildren(ulId);
-  var node = document.createElement('LI'); // Create a <li> node
-  var header2 = document.createElement('h2');
+  var node = document.createElement("LI"); // Create a <li> node
+  var header2 = document.createElement("h2");
   header2.textContent = heading;
   node.appendChild(header2); // Append the text to <li>
   ulId.appendChild(node);
   wallObj.forEach(function (user, counter) {
-    var innerNode = document.createElement('LI'); // Create a <li> node
-    var image = document.createElement('IMG');
-    var header = document.createElement('h3');
-    header.textContent = String(counter + 1) + '.  ' + String(user.git_username);
+    var innerNode = document.createElement("LI"); // Create a <li> node
+    var image = document.createElement("IMG");
+    var header = document.createElement("h3");
+    header.textContent = String(counter + 1) + ".  " + String(user.git_username);
     image.src = user.git_photo_url;
-    image.alt = String(user.git_username) + ' GIT Profile Photo';
+    image.alt = String(user.git_username) + " GIT Profile Photo";
     innerNode.appendChild(image);
     innerNode.appendChild(header); // Append the text to <li>
     ulId.appendChild(innerNode);
@@ -74,40 +82,99 @@ var generateWallHTML = function generateWallHTML(wallObj, ulId, heading) {
 var newWalls = function newWalls() {
   getWalls(function (err, wallsObject) {
     if (err) {
-      warningMessage('Error while getting walls', 2000);
+      warningMessage("Error while getting walls", 2000);
     } else {
       var wallOfFame = wallsObject.wallOfFame,
           wallOfShame = wallsObject.wallOfShame;
 
-      var wallOfFameUL = document.getElementById('wall-of-fame');
-      var wallOfShameUL = document.getElementById('wall-of-shame');
-      generateWallHTML(wallOfFame, wallOfFameUL, 'Wall of Fame');
-      generateWallHTML(wallOfShame, wallOfShameUL, 'Wall of Shame');
+      var wallOfFameUL = document.getElementById("wall-of-fame");
+      var wallOfShameUL = document.getElementById("wall-of-shame");
+      generateWallHTML(wallOfFame, wallOfFameUL, "Wall of Fame");
+      generateWallHTML(wallOfShame, wallOfShameUL, "Wall of Shame");
     }
   });
 };
 
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   newQuiz();
   newWalls();
 });
 
 var postQuizResults = function postQuizResults(winnerProp) {
   // The only possible properties are user1 and user2
-  var loserProp = winnerProp === 'user1' ? 'user2' : 'user1';
-  var winnerLoserObj = { winner: globalUsers[winnerProp], loser: globalUsers[loserProp] };
+  var loserProp = winnerProp === "user1" ? "user2" : "user1";
+  var winnerLoserObj = {
+    winner: globalUsers[winnerProp],
+    loser: globalUsers[loserProp]
+  };
   postQuizRes(winnerLoserObj, function (err) {
     if (err) {
-      warningMessage('Sorry, there was a problem with sending your answer', 3000);
+      warningMessage("Sorry, there was a problem with sending your answer", 3000);
     }
   });
   newQuiz();
   newWalls();
 };
 
-user1.addEventListener('click', function () {
-  return postQuizResults('user1');
+user1.addEventListener("click", function () {
+  return postQuizResults("user1");
 }, true);
-user2.addEventListener('click', function () {
-  return postQuizResults('user2');
+user2.addEventListener("click", function () {
+  return postQuizResults("user2");
 }, true);
+
+/*function for checking the user's validation*/
+
+var checkEmail = function checkEmail() {
+  if (email.validity.typeMismatch) {
+    console.log(email.validity.typeMismatch);
+    displayErr(emailErr, "Please enter a valid email address");
+  } else if (email.validity.valueMissing) {
+    displayErr(emailErr, "Please enter an email address");
+  } else {
+    displayErr(emailErr, "");
+    return true;
+  }
+};
+
+var checkPw = function checkPw() {
+  if (password.validity.patternMismatch) {
+    displayErr(passwordErr, "Password must contain at least eight characters, including one letter and one number");
+  } else if (password.validity.valueMissing) {
+    displayErr(passwordErr, "Please enter a password");
+  } else {
+    displayErr(passwordErr, "");
+    return true;
+  }
+};
+
+var checkConfirmPw = function checkConfirmPw() {
+  if (password.value != confirmPassword.value) {
+    displayErr(confirmErr, "Passwords do not match");
+  } else if (confirmPassword.validity.valueMissing) {
+    displayErr(confirmErr, "Please confirm your password");
+  } else {
+    displayErr(confirmErr, "");
+    return true;
+  }
+};
+
+function displayErr(errElem, errMsg) {
+  errElem.innerText = errMsg;
+}
+
+email.addEventListener("focusout", checkEmail);
+password.addEventListener("focusout", checkPw);
+confirmPassword.addEventListener("focusout", checkConfirmPw);
+
+form.addEventListener("submit", function (event) {
+  if (!checkEmail()) {
+    event.preventDefault();
+  }
+  if (!checkPw()) {
+    event.preventDefault();
+  }
+  if (!checkConfirmPw()) {
+    event.preventDefault();
+  }
+});
