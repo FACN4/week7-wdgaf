914c45be3e0f6d35fc5b636dcd936f740c2a201d
Matt Hall <matthall8@gmail.com>
2018-08-14 17:29:20 +0300

    Updated build.js and build.sql

--- src/database/build.js
+++ src/database/build.js
@@ -13,13 +13,12 @@ const runDbBuild = (cb) => {
     } else {
       userData.forEach((person) => {
         if (person.avatar_url) {
-          const SQLquery = 'INSERT INTO users (git_username,git_profile_url,git_photo_url) VALUES ($1,$2,$3)';
-          dbConnection.query(SQLquery, [person.login, person.html_url, person.avatar_url],
-            (err) => {
-              if (err) {
-                cb(err);
-              }
-            });
+          const SQLquery = 'INSERT INTO git_profiles (git_username,git_profile_url,git_photo_url) VALUES ($1,$2,$3)';
+          dbConnection.query(SQLquery, [person.login, person.html_url, person.avatar_url], (err) => {
+            if (err) {
+              cb(err);
+            }
+          });
         }
       });
       cb(null);
