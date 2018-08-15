3053654c4c5c612e8b2a4c17f18f5bd979a3ff28
afifisuha <afifisuha@campus.technion.ac.il>
2018-08-15 13:06:01 +0300

    #1 login post xhr and dom

--- src/query/getData.js
+++ src/query/getData.js
@@ -2,15 +2,17 @@ const dbConnection = require('../database/dbconnection.js');
 
 // Gets everything from the users table and returns it in a callback
 const getUserData = (cb) => {
-  dbConnection.query('SELECT * FROM git_profiles ORDER BY elo_ranking DESC', (err, res) => {
-    if (err) {
-      cb(err);
-    } else {
-      cb(null, res.rows);
-    }
-  });
+  dbConnection.query(
+    'SELECT * FROM git_profiles ORDER BY elo_ranking DESC',
+    (err, res) => {
+      if (err) {
+        cb(err);
+      } else {
+        cb(null, res.rows);
+      }
+    },
+  );
 };
-
 const getHash = (cb, userName) => {
   dbConnection.query(
     'SELECT password FROM users WHERE $userName ORDER BY elo_ranking DESC',
