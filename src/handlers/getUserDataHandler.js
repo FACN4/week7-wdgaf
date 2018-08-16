6a23c1e1ca409c34a260708aa79cdea6959a9f13
afifisuha <afifisuha@campus.technion.ac.il>
2018-08-16 16:31:09 +0300

    #25 secret now is removed from handlers files

--- src/handlers/getUserDataHandler.js
+++ src/handlers/getUserDataHandler.js
@@ -1,15 +1,15 @@
 const cookie = require('cookie');
 const { verify } = require('jsonwebtoken');
 
-const SECRET = 'hcdcjdhcdcdchdohcioj';
-
 const getUserDataHandler = (request, response) => {
   if (request.headers.cookie) {
     const { jwt } = cookie.parse(request.headers.cookie);
     verify(jwt, SECRET, (err, username) => {
       if (err) {
         response.writeHead(500, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify({ error: 'Sorry, unable to fulfil request' }));
+        response.end(
+          JSON.stringify({ error: 'Sorry, unable to fulfil request' }),
+        );
       } else {
         response.writeHead(200, { 'Content-Type': 'application/json' });
         response.end(JSON.stringify(username));
